a9ade014bf1ed1a98e1adf6b3a67815ab337a67b
Lossani <dev@xempre.com>
2021-11-24 02:42:53 -0500

    Release: Final preparation for Tomcat deployment.

--- src/main/java/com/gamingworld/app/gamingworld/GamingworldApplication.java
+++ src/main/java/com/gamingworld/app/gamingworld/GamingworldApplication.java
@@ -2,12 +2,19 @@ package com.gamingworld.app.gamingworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GamingworldApplication {
+public class GamingworldApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GamingworldApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(GamingworldApplication.class);
 	}
 
 }
